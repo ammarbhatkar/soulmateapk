d7f56426075e62cd15458a9057254b7c8b77e696
ammarbhatkar <36763132+ammarbhatkar@users.noreply.github.com>
2024-02-06 23:03:42 +0530

    updated_auth_gate
    navigation issue solved

--- lib/services/auth_gate.dart
+++ lib/services/auth_gate.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:soulmateapk/pages/add_first_name.dart';
@@ -13,17 +14,39 @@ class AuthGate extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final FirebaseAuth _auth = FirebaseAuth.instance;
-    final ChatServices _chatServices = ChatServices();
     return Scaffold(
       body: Container(
         color: Colors.black,
         child: Center(
           child: StreamBuilder(
-            stream: _chatServices.getUserStream(),
+            stream: FirebaseAuth.instance.authStateChanges(),
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                return MainPage();
+                User? user = FirebaseAuth.instance.currentUser;
+                return FutureBuilder<DocumentSnapshot>(
+                  future: FirebaseFirestore.instance
+                      .collection('Users')
+                      .doc(user!.uid)
+                      .get(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      DocumentSnapshot<Object?>? userDoc = snapshot.data;
+                      var userData = userDoc!.data() as Map<String, dynamic>?;
+                      if (userDoc.exists &&
+                          userData != null &&
+                          userData.containsKey('profileSetup') &&
+                          userData['profileSetup'] == true) {
+                        return MainPage();
+                      } else {
+                        return AddFirstName(); // or any other page where the user can enter their details
+                      }
+                    } else if (snapshot.hasError) {
+                      return Text("Error: ${snapshot.error}");
+                    } else {
+                      return CircularProgressIndicator(); // show a loading spinner while waiting for the data
+                    }
+                  },
+                );
               } else {
                 return LoginPage();
               }
